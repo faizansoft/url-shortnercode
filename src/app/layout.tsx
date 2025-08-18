a9e7698ade4afbb84f6a17be2fc6ea48a640171f
faizansoft <126218544+faizansoft@users.noreply.github.com>
2025-08-19 01:06:44 +0500

    fd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,11 +5,15 @@ import "./globals.css";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+  preload: false, // avoid preloaded-but-not-used warnings
+  display: 'swap',
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  preload: false,
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
