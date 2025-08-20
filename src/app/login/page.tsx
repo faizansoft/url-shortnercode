9ebd9901b801183fab4b4223dfcccd40d5815dd9
faizansoft <126218544+faizansoft@users.noreply.github.com>
2025-08-20 17:15:49 +0500

    sd

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,6 @@
+export const runtime = 'edge'
+export const dynamic = 'force-dynamic'
+
 import { redirect } from "next/navigation";
 import { getSupabaseServerClient } from "@/lib/supabaseServerSSR";
 import LoginClient from "./LoginClient";
