c182b5a83c76d7fb943d7ffe6ab4051b98eb2d19
faizansoft <126218544+faizansoft@users.noreply.github.com>
2025-08-21 15:35:01 +0500

    sd

--- src/app/[code]/route.ts
+++ src/app/[code]/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { getSupabaseServer } from '@/lib/supabaseServer'
 
-export const runtime = 'edge'
+export const runtime = 'nodejs'
 export const dynamic = 'force-dynamic'
 
 export async function GET(
