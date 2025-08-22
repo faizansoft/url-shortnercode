1dab206dc506ed30288cc01a4685faf8995e5c0e
faizansoft <126218544+faizansoft@users.noreply.github.com>
2025-08-22 16:24:37 +0500

    gh

--- src/app/i/[code]/page.tsx
+++ src/app/i/[code]/page.tsx
@@ -4,3 +4,6 @@ export default async function Page({ params }: { params: Promise<{ code: string 
   const { code } = await params
   return <InterstitialClient code={code} />
 }
+
+export const runtime = 'edge'
+export const dynamic = 'force-dynamic'
